cf7841aa71ef341c77cd853eeb074ddbaaf95981
Marcus Hughes <marcushughes80@gmail.com>
2018-03-19 00:40:16 -0400

    Update SDKVERSION

--- app/src/main/java/com/umusic/marcus/umusic/ui/library/LibraryActivity.kt
+++ app/src/main/java/com/umusic/marcus/umusic/ui/library/LibraryActivity.kt
@@ -1,13 +1,14 @@
 package com.umusic.marcus.umusic.ui.library
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.umusic.marcus.umusic.R
+import com.umusic.marcus.umusic.ui.BaseActivity
 
-class LibraryActivity : AppCompatActivity() {
+class LibraryActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
+        intNav()
     }
 }
